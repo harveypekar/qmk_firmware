caa0cfd4fd1e86fe0ebb6ab674ff70d241697964
Daan Nijs <nijs.daan@gmail.com>
2018-02-21 18:03:08 +0100

    wip

--- keyboards/ergodox_ez/keymaps/elmarcel/keymap.c
+++ keyboards/ergodox_ez/keymaps/elmarcel/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Del    |   Q  |   W  |   F  |   P  |   G  |  L1  |           |  L1  |   J  |   L  |   U  |   Y  |   ;  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   R  |   S  |T(SFT)|   D  |------|           |------|   H  |N(SFT)|   E  |   I  |   O  |' / Cmd |
+ * | BkSp   |   A  |   R  |S     |T(LSF)|   D  |------|           |------|   H  |N(SFT)|   E  |   I  |   O  |' / Cmd |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   K  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -40,20 +40,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DELT,        KC_Q,         KC_W,   KC_F,   KC_P,   KC_G,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_R,   KC_S,   KC_T,   KC_D,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_EQL,         KC_1,         KC_2,             KC_3,               KC_4,                KC_5,   KC_LEFT,
+        KC_DELT,        KC_Q,         KC_W,             KC_F,               KC_P,                KC_G,   TG(SYMB),
+        KC_BSPC,        KC_A,         MT(MOD_LALT,KC_R),MT(MOD_LCTL,KC_S),  MT(MOD_LSFT,KC_T),   KC_D,
+        //KC_BSPC,        KC_A,         KC_R,    KC_S,        KC_T,         KC_D,        
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,             KC_C,               KC_V,                KC_B,   ALL_T(KC_NO),
+        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),    KC_LEFT,            KC_RGHT,
+
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_J,   KC_L,  KC_U,   KC_Y,   KC_SCLN,             KC_BSLS,
-                          KC_H,   KC_N,  KC_E,   KC_I,   KC_O,      GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_K,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+             KC_RGHT,     KC_6,   KC_7,                KC_8,              KC_9,              KC_0,             KC_MINS,
+             TG(SYMB),    KC_J,   KC_L,                KC_U,              KC_Y,              KC_SCLN,             KC_BSLS,
+                          KC_H,   MT(MOD_RSFT, KC_N) , MT(MOD_RCTL,KC_E), MT(MOD_RALT, KC_I),KC_O,      GUI_T(KC_QUOT),
+             MEH_T(KC_NO),KC_K,   KC_M,                KC_COMM,           KC_DOT,            CTL_T(KC_SLSH),   KC_RSFT,
+                                  KC_UP,               KC_DOWN,           KC_LBRC,           KC_RBRC,          KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_TAB, KC_ENT
